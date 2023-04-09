a7a31356657547aea76adec3c767c009a8f3272e
Ivan <i.a.konuhov@gmail.com>
2023-04-09 21:16:47 +0700

    Initial Commit

---
+++ SimpleCoreData/SimpleCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCoreDataApp.swift
+//  SimpleCoreData
+//
+//  Created by Иван on 09.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
